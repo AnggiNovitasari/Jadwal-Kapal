f463abaa2a186431efd309dc3dfdd3f6bc76eeca
febrianto05 <febryblctelkom@gmail.com>
2019-02-26 16:38:57 +0700

    versi 1.0

---
+++ app/src/main/java/com/example/anonymous/jadwalkapal/jadwal_kapal.java
@@ -0,0 +1,13 @@
+package com.example.anonymous.jadwalkapal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jadwal_kapal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal_kapal);
+    }
+}
